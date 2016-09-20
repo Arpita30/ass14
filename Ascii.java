4c2497bdf2cfef026540c52317a1062d6a6e5c2c
Arpita30 <arpita.salecha.jain@gmail.com>
2016-09-20 19:00:57 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,17 @@
+
+public class Ascii {
+
+	
+	public static void main(String[] args) {
+		char alpha;
+		
+		for(int num=65;num<=90;num++)
+		{
+			alpha=(char)num;
+			System.out.println(num+"-"+alpha);
+			
+		}
+
+	}
+
+}
